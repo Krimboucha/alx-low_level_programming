71fc2637168bc6c7dcbfef2b412eb0323659b6e3
Krimboucha <hamza.berraho.ma@gmail.com>
2023-03-23 16:14:12 -0700

    let's get back to it

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,9 +1,14 @@
 #ifndef MAIN_H_INCLUDED
 #define MAIN_H_INCLUDED
 /**
- * _isupper - checks if character is 
- * uppercase or not
- * @c: character to check
- * Return: 1 or 0
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int _isupper(int c);
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
  */
 int _isupper(int c);
