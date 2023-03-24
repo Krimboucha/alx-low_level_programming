b1114e4fe7b3e8dd2922558939cafd11a05597e7
Krimboucha <hamza.berraho.ma@gmail.com>
2023-03-23 19:06:23 -0700

    let's get back to it

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,18 +9,18 @@ void print_diagonal(int n)
 	int j;
 
 	if (n <= 0)
-    	{
-        	_putchar('\n');
-        	return;
-    	}
+	{
+		_putchar('\n');
+		return;
+	}
 
-    	for (i = 0; i < n; i++)
-    	{
-        	for (j = 0; j < i; j++)
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
 		{
-        		_putchar(' ');
-        	}
-        	_putchar('\\');
-        	_putchar('\n');
-    	}
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
 }
