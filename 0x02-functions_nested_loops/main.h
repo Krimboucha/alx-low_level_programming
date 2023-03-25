51de9277c3b782968b0cd0d4b68932b74b6f26d4
Krimboucha <hamza.berraho.ma.com>
2023-03-25 11:44:28 -0700

    I see you O.O

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -15,4 +15,11 @@ void print_alphabet(void);
  * print_alphabet_x10 - prints the alphabet in lower case 10 times
  */
 void print_alphabet_x10(void);
+/**
+ * _islower - checks if character is lowercase
+ * @c: character to be checked
+ *
+ * Return: 1 for lowercase or 0 if not
+ */
+int _islower(int c);
 #endif
