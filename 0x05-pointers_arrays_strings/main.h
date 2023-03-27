95171a57b5d092a72f1670556a6b2cc54b5079e9
Krimboucha <hamza.berraho.ma.com>
2023-03-27 16:37:44 -0700

    C - Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+
+/**
+ * reset_to_98 - resets pointer to 98
+ * Return void
+ */
+
+void reset_to_98(int *n);
+
+/**
+ * swap_int - swaps the values of two integers
+ * return: void
+ */
+
+void swap_int(int *a, int *b);
+#endif
