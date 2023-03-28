00f4926d5b3222104f774796a64be585b72afada
Krimboucha <hamza.berraho.ma.com>
2023-03-27 17:02:32 -0700

    C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,8 +3,8 @@
 
 /**
  * reset_to_98 - resets pointer to 98
- * @a first pointer to int to switch
- * @b second pointer to int to switch
+ * @a: first pointer to int to switch
+ * @b: second pointer to int to switch
  * Return void
  */
 
@@ -12,7 +12,7 @@ void reset_to_98(int *n);
 
 /**
  * swap_int - swaps the values of two integers
- * @n pointer to int
+ * @n: pointer to int
  * return: void
  */
 
@@ -24,4 +24,12 @@ void swap_int(int *a, int *b);
  * Return: length of string
  */
 int _strlen(char *s);
+
+/**
+ * _puts - prints a string followed by a new
+ * @str: pointer to char
+ * Return: void
+ */
+
+void _puts(char *str);
 #endif
