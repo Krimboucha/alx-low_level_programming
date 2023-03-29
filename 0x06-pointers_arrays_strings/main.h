634acecc338a185d4c0d0d364a288ffe43b3075e
Krimboucha <hamza.berraho.ma.com>
2023-03-29 14:54:21 -0700

    C - More pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: pointer to char
+ * @sr: pointer to char
+ *
+ * return: dest string
+ */
+
+char *_strcat(char *dest, char *src);
+#endif
