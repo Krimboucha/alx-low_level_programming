bbeab8dec01842b84532fc4e507664d13dc289ff
Krimboucha <hamza.berraho.ma.com>
2023-03-30 16:12:42 -0700

    C - More pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * int _strcmp - compares two strings
+ * @s1: first string to be compared
+ * @s2: second string to be compared
+ * 
+ * Return: 0 if the two strings r similar, >0 if s1>s2 in ASCII, <0 if s1<s2 in ASCII
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int counter = 0;
+
+	while (*s1 && (*s1 == *s2))
+	{
+		s1++;
+		s2++;
+	}
+	if (*s1 > *s2)
+	{
+		counter++;
+	}
+	else if (*s1 < *s2)
+	{
+		counter--;
+	}
+	return counter;
+}
