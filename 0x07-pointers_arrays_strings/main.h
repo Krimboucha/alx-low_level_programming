b4d07d100e545b4997033b8e861d05e5f28797d6
Krimboucha <hamza.berraho.ma.com>
2023-04-03 14:08:52 -0700

    C - Even more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -10,4 +10,14 @@
  */
 
 char *_memset(char *s, char b, unsigned int n);
+
+/**
+ * _memcpy - copies memory area
+ * @dest: pointer to char
+ * @src: pointer to char
+ * @n: int
+ * Return: pointer to char
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n);
 #endif
