9c99d836d16298972e436b2333e69e24eb3b4872
Krimboucha <hamza.berraho.ma.com>
2023-04-03 15:30:15 -0700

    C - Even more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
+ * _strspn - gets the length of a prefix substring
+ * @s: pointer to char
+ * @accept: pointer to char
  *
- *
- *
- *
- *
+ * Return: int
  */
 
 unsigned int _strspn(char *s, char *accept)
@@ -15,17 +15,20 @@ unsigned int _strspn(char *s, char *accept)
 	int c;
 
 	c = 0;
-	for (i = 0; s[i] != '\0'; i++) {
-		for (j = 0; accept[j] != '\0'; j++) {
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
 			if (s[i] == accept[j])
 	    		{
 				c++;
 				break;
 			}
 		}
-		if (accept[j] == '\0') {
-			return c;
+		if (accept[j] == '\0')
+		{
+			return (c);
 		}
 	}
-	return c;
+	return (c);
 }
