33bb3195d163808256b4cd0eed123fe26d98429d
Krimboucha <hamza.berraho.ma.com>
2023-04-05 13:49:47 -0700

    C - Recursion

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -37,4 +37,13 @@ void _print_rev_recursion(char *s);
  */
 
 int _strlen_recursion(char *s);
+
+/*
+ * factorial - returns the factorial of a given number
+ * @n: int
+ *
+ * Return: int
+ */
+
+int factorial(int n);
 #endif
