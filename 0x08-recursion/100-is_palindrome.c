d2fd5f6c4a50c06f02046d9491d9fbc122fb2d2a
Krimboucha <hamza.berraho.ma.com>
2023-04-06 14:59:11 -0700

    C - Recursion

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,5 +1,38 @@
 #include "main.h"
 #include <string.h>
+int _strlen_recursion(char *s);
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: pointer to char
+ *
+ * Return: int
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		s++;
+		return (1 + _strlen_recursion(s));
+	}
+}
+
+/*
+ * real_palindrome - checks if a string is palindrome
+ * @s: pointer on char
+ * @i: int
+ *
+ * Return: 1 if palindrome, 0 if not
+ */
+
+int real_palindrome(char *s, int i)
+{
+	
+}
 
 /**
  * is_palindrome - checks if a string is palindrome
@@ -12,8 +45,9 @@ int is_palindrome(char *s)
 {
 	int test;
 	int len;
+	/*char *p = s;*/
 
-	len = strlen(s);
+	len = _strlen_recursion(s);
 	if (len == 0 || len == 1)
 	{
 		return (1);
--- 0x08-recursion/100-main.c
+++ 0x08-recursion/100-main.c
@@ -1,7 +1,6 @@
 #include "main.h"
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+
 /**
  * main - check the code
  *
@@ -9,19 +8,15 @@
  */
 int main(void)
 {
-	int r;
-	char *s;
-	s = (char *)malloc(10*sizeof(char));
-	strcpy(s,"level");
-	r = is_palindrome(s);
-	printf("%d\n", r);
-/**    r = is_palindrome("level");
- *   printf("%d\n", r);
-  *  r = is_palindrome("redder");
-   * printf("%d\n", r);
-   * r = is_palindrome("test");
-   * printf("%d\n", r);
-   * r = is_palindrome("step on no pets");
-   * printf("%d\n", r);
-   */ return (0);
+    int r;
+
+    r = is_palindrome("level");
+    printf("%d\n", r);
+    r = is_palindrome("redder");
+    printf("%d\n", r);
+    r = is_palindrome("test");
+    printf("%d\n", r);
+    r = is_palindrome("step on no pets");
+    printf("%d\n", r);
+    return (0);
 }
