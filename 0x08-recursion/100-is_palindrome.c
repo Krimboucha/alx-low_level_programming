99c2683ebad0215e145e9985f105c413e48dc688
Krimboucha <hamza.berraho.ma.com>
2023-04-05 19:36:49 -0700

    C - Recursion

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,11 +1,13 @@
 #include "main.h"
 #include<string.h>
+
 /**
  * is_palindrome - checks if a string is palindrome
  * @s: pointer to char
  *
  * Return: 1 if it is, 0 if not
  */
+
 int is_palindrome(char *s)
 {
 	int test;
@@ -16,15 +18,11 @@ int is_palindrome(char *s)
 	{
 		return (1);
 	}
-	else if (s[0] != s[len - 1])
+	if (s[0] != s[len - 1])
 	{
 		return (0);
 	}
-	else
-	{
-		s[len - 1] = '\0';
-		test = is_palindrome(&s[1]);
-		s[len - 1] = s[0];
-		return (test);
-    }
+	s[len - 1] = '\0';
+	test = is_palindrome(s + 1);
+	return (test);
 }
