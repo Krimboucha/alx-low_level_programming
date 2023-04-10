ebb44def5ab7f4315846ea62e0f2eddbf91014c9
Krimboucha <hamza.berraho.ma.com>
2023-04-10 12:28:15 -0700

    C - Recursion

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,8 +9,8 @@
  * Return: EXIT_SUCCESS or EXIT_FAILURE
  */
 
-int main(int argc, char *argv[])
+int main(int __attribute__((unused)) argc, char *argv[])
 {
-	printf("%s\n", argv[argc-argc]);
+	printf("%s\n", argv[0]);
 	return (EXIT_SUCCESS);
 }
