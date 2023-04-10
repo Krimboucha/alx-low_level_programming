a93103c594047dcf62a47ed8c7cb4d06d16219fe
Krimboucha <hamza.berraho.ma.com>
2023-04-10 09:57:07 -0700

    C - Recursion

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints its name
+ * @argc: number of command line arguments
+ * @argv: arrays of these arguments
+ *
+ * Return: int
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return EXIT_SUCCESS;
+}
