23b8560fc53882b2fb93c90098b4deb222531ae1
Krimboucha <hamza.berraho.ma.com>
2023-04-11 16:29:30 -0700

    C - malloc, free

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -21,4 +21,13 @@ char *create_array(unsigned int size, char c);
  */
 
 char *_strdup(char *str);
+
+/** str_concat - concatenates two strings
+ * @s1: pointer to char
+ * @s2: pointer to char
+ *
+ * Return: pointer to char
+ */
+
+char *str_concat(char *s1, char *s2);
 #endif
