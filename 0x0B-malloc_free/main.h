75caefb00de508eec5c1090c847bc29e63512860
Krimboucha <hamza.berraho.ma.com>
2023-04-11 15:41:04 -0700

    C - malloc, free

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+
+/**
+ * create_array - creates an array of chars
+ * and initializes it with a specific char
+ * @size: unsigned int
+ * @c: char
+ * 
+ * Return: pointer to char
+ */
+
+char *create_array(unsigned int size, char c);
+#endif
