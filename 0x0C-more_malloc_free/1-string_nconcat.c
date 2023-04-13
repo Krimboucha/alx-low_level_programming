92af2583d3282949ed27c089e33539c3e7e5d6f7
Krimboucha <hamza.berraho.ma.com>
2023-04-13 15:00:30 -0700

    C - More  malloc, free

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,4 +1,4 @@
-#include "main."
+#include "main.h"
 #include <stdlib.h>
 #include <string.h>
 char *_memcpy(char *dest, char *src, unsigned int n);
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -9,4 +9,23 @@
  */
 
 void *malloc_checked(unsigned int b);
+/**
+ * string_nconcat - concatenates two strings
+ * @s1: pointer to char
+ * @s2: pointer to char
+ * @n: unsigned int
+ *
+ * Return: pointer to char
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+/**
+ * _memcpy - copies n bytes from memory area src to memory area dest
+ * @dest: memory area to to which n bytes of src will be copied
+ * @src: memory area from which n bytes will be copied
+ * @n: number of bytes to be copied
+ *
+ * Return: pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n);
 #endif
