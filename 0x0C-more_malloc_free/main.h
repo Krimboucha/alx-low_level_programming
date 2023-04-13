7fb9f1160b8c1ed0e1612811ef0b0edbed6ea4d8
Krimboucha <hamza.berraho.ma.com>
2023-04-13 14:02:30 -0700

    C - More  malloc, free

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,3 +1,12 @@
 #ifndef MAIN_H_INCLUDED
 #define MAIN_H_INCLUDED
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: unsigned int
+ *
+ * Return: pointer to the allocated memory
+ */
+
+void *malloc_checked(unsigned int b);
 #endif
