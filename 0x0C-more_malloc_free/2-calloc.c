35689855728da93dbd3ba59626f9e0c7e0de315c
Krimboucha <hamza.berraho.ma.com>
2023-04-13 19:32:44 -0700

    C - More  malloc, free

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,14 +13,10 @@ void* _calloc(unsigned int nmemb, unsigned int size)
 	void *ptr;
 
 	if (nmemb == 0 || size == 0)
-	{
 		return (NULL);
-	}
 	ptr = malloc(nmemb * size);
 	if (ptr == NULL)
-	{
 		return (NULL);
-	}
 	_memset(ptr, 0, nmemb * size);
 	return (ptr);
 }
