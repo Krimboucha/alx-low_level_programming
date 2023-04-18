3ca1a0b3bf9a43ae90b0154a19c5bd55a61f5a51
Krimboucha <hamza.berraho.ma.com>
2023-04-18 16:10:59 -0700

    C - Structures, typedef

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,28 +10,31 @@
 
 void print_dog(struct dog *d)
 {
-	if (d->name != NULL)
+	if (d != NULL)
 	{
-		printf("Name: %s\n", d->name);
-	}
-	else
-	{
-		printf("Name: (nil)\n");
-	}
-	if (d->age != 0)
-	{
-		printf("Age: %f\n", d->age);
-	}
-	else
-	{
-		printf("Age: 0\n");
-	}
-	if (d->owner != NULL)
-	{
-		printf("Owner: %s\n", d->owner);
-	}
-	else
-	{
-		printf("Owner: (nil)\n");
+		if (d->name != NULL)
+		{
+			printf("Name: %s\n", d->name);
+		}
+		else
+		{
+			printf("Name: (nil)\n");
+		}
+		if (d->age != 0)
+		{
+			printf("Age: %f\n", d->age);
+		}
+		else
+		{
+			printf("Age: 0\n");
+		}
+		if (d->owner != NULL)
+		{
+			printf("Owner: %s\n", d->owner);
+		}
+		else
+		{
+			printf("Owner: (nil)\n");
+		}
 	}
 }
