b15e09733b36eed2dcdd363beed79e486f248900
Krimboucha <hamza.berraho.ma.com>
2023-04-18 15:50:54 -0700

    C - Structures, typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - creates dogs ig
+ * @name: dog's name
+ * @age:  dog's age
+ * @owner:  dog owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
