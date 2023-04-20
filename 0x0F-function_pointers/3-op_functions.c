651ae9ec0ebcc7e330e474c5a67c908edf577bb7
Krimboucha <hamza.berraho.ma.com>
2023-04-19 17:33:51 -0700

    C - Function pointers

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -55,11 +55,6 @@ int op_mul(int a, int b)
 
 int op_div(int a, int b)
 {
-	if (b == 0)
-	{
-		printf("Error: division by zero\n");
-		exit(100);
-	}
 	return (a / b);
 }
 
@@ -73,11 +68,6 @@ int op_div(int a, int b)
 
 int op_mod(int a, int b)
 {
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
 	return (a % b);
 }
 
