ebef37fc9c4b823164bd509c6afa824dc2c05920
Krimboucha <hamza.berraho.ma.com>
2023-04-20 13:59:44 -0700

    C - Variadic functions

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,15 +10,14 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	int sum;
+	unsigned int i;
 	va_list args;
-	int i;
 
+	sum = 0;
 	if (n == 0)
 	{
 		return (0);
 	}
-	sum = 0;
-	args;
 	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+#include <stdarg.h>
+
+/**
+ * sum_them_all - sum of all its parameters
+ * @n: int
+ *
+ * Return: int
+ */
+
+int sum_them_all(const unsigned int n, ...);
+#endif
