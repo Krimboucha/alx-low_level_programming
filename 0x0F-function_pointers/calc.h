6f6e5415da43de10ff3d3f3084be79fc6b71e9ef
Krimboucha <hamza.berraho.ma.com>
2023-04-19 17:02:57 -0700

    C - Function pointers

---
+++ 0x0F-function_pointers/calc.h
@@ -0,0 +1,50 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+
+/**
+ * op_add - adds two numbers
+ * @a: int
+ * @b: int
+ *
+ * Return: int
+ */
+
+int op_add(int a, int b);
+/**
+ * op_sub - substraction
+ * @a: int
+ * @b: int
+ *
+ * Return: int
+ */
+
+int op_sub(int a, int b);
+
+/**
+ * op_mul - multiplicates two numbers
+ * @a: int
+ * @b: int
+ *
+ * Return: int
+ */
+
+int op_mul(int a, int b);
+/**
+ * op_div - divides two numbers
+ * @a: int
+ * @b: int
+ *
+ * Return: int
+ */
+
+int op_div(int a, int b);
+
+/**
+ * op_mod - does what mod does to two numbers
+ * @a: int
+ * @b: int
+ *
+ * Return: int
+ */
+
+int op_mod(int a, int b);
