4c8bd31f92a0e93c8a70d231172da38d96fee97c
Krimboucha <hamza.berraho.ma.com>
2023-04-19 17:30:57 -0700

    C - Function pointers

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "calc.h"
-
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
 /**
  * op_add - adds two numbers
  * @a: int
@@ -71,7 +75,7 @@ int op_mod(int a, int b)
 {
 	if (b == 0)
 	{
-		printf("Error: modulo by zero\n");
+		printf("Error\n");
 		exit(100);
 	}
 	return (a % b);
