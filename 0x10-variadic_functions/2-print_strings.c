db2ba9f65ab34569348ca56cad4af0383143d59a
Krimboucha <hamza.berraho.ma.com>
2023-04-21 18:28:52 -0700

    C - Variadic functions

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,22 +1,29 @@
-#include <stdarg.h>
-#include <stdio.h>
 #include "variadic_functions.h"
-
 /**
- * print_numbers -  prints numbers, followed by a new line
- * @separator: pointer to char
+ * print_strings - prints a string
+ * @separator: char
+ * @n: int
  *
  * Return: void
  */
-void print_numbers(const char *separator, const unsigned int n, ...)
+void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
+	char *ptr;
 	va_list args;
 
 	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", va_arg(args, int));
+		ptr = va_arg(args, char*);
+		if (ptr == NULL)
+		{
+			printf("(nil)");
+		}
+		else
+		{
+			printf("%s", ptr);
+		}
 		if (separator == NULL)
 		{
 			continue;
