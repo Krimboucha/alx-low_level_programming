daa9075deeb98bd5f09f995bba1f4a0d52afc166
Krimboucha <hamza.berraho.ma@gmail.com>
2023-04-27 15:42:37 -0700

    C - Singly linked lists

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -20,6 +20,12 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	}
 	ptr->str = strdup(str);
+	if (ptr->str == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	ptr->len = strlen(str);
 	ptr->next = *head;
 	*head = ptr;
 	return (ptr);
