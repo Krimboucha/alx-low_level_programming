0b875e5a57c4c7b1e715c57be066902235540134
Krimboucha <hamza.berraho.ma@gmail.com>
2023-04-28 12:08:38 -0700

    C - Singly linked lists

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * first - prints before the main is executed
+ *
+ * Return: void
+ */
+
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
