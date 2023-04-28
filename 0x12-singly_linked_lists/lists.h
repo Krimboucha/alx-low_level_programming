0e67c6bc68f2dc80d659e6ac901209aa9bcc5f08
Krimboucha <hamza.berraho.ma@gmail.com>
2023-04-28 12:10:36 -0700

    C - Singly linked lists

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -68,4 +68,12 @@ list_t *add_node_end(list_t **head, const char *str);
  */
 
 void free_list(list_t *head);
+
+/**
+ * first - prints before the main is executed
+ *
+ * Return: void
+ */
+
+void first(void) __attribute__((constructor));
 #endif
