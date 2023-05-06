40a4f25dd17998230f9fa7a81927ed1fc7c410df
Krimboucha <hamza.berraho.ma@gmail.com>
2023-05-05 17:21:21 -0700

    C - Bit manipulation

--- 0x14-bit_manipulation/test.c
+++ 0x14-bit_manipulation/test.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
 
 /** print_binary - prints the binary representation number
  * @n: UL
@@ -10,24 +10,15 @@
 void print_binary(unsigned long int n)
 {
 	unsigned long int mask;
-	int start;
 
 	mask = 1UL << (sizeof(unsigned long int) * 8 - 1);
-	start = 0;
-	if (n == 0)
-	{
-		_putchar('0');
-		_putchar('\n');
-		return;
-	}
 	while (mask > 0)
 	{
 		if (n & mask)
 		{
-			start = 1;
 			_putchar('1');
 		}
-		else if (start)
+		else
 		{
 			_putchar('0');
 		}
@@ -35,4 +26,3 @@ void print_binary(unsigned long int n)
 	}
 	_putchar('\n');
 }
-
