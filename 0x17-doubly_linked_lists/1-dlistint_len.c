4fb3bf375e29f98dfb0176d296c59bba8d87a040
Krimboucha <hamza.berraho.ma@example.com>
2023-08-17 20:56:17 +0100

    betty

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,12 +1,10 @@
 #include<stdio.h>
 #include "lists.h"
-
 /** dlistint_len -  returns length of a list
  * @h: pointer
  *
  * Return: size_t
  */
-
 size_t dlistint_len(const dlistint_t *h)
 {
 	size_t count;
