d829fe5daa84ea293715f259245c526bd1cceec3
Krimboucha <hamza.berraho.ma@example.com>
2023-08-17 20:27:51 +0100

    betty

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -2,7 +2,7 @@
 #include "lists.h"
 
 /**
- * print_dlistint - prints a doubly linked list
+ * print_dlistint: prints a doubly linked list
  * @h: pointer
  *
  * Return: size_t
@@ -18,5 +18,5 @@ size_t print_dlistint(const dlistint_t *h)
 		h = h->next;
 		count++;
 	}
- 	return (count);
+	return (count);
 }
