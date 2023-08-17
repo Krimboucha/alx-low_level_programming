38b07e9fcf3a54432e2ea668470017404b32e570
Krimboucha <hamza.berraho.ma@example.com>
2023-08-17 20:42:49 +0100

    betty

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,6 +1,12 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include"lists.h"
+/**
+ * free_dlistint - ...
+ * @head: ...
+ *
+ * Return: void
+ */
 void free_dlistint(dlistint_t *head)
 {
 	while (head != NULL)
