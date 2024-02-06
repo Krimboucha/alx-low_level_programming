0b34c041d73729d747c61af124c3cd73a0aa47dd
Krimboucha <hamza.berraho.ma@example.com>
2024-02-06 17:09:12 +0100

    test

---
+++ 0x1A-hash_tables/0-test.c
@@ -0,0 +1,13 @@
+#include "hash_tabls.h"
+
+/**
+ * hash_table_creat - creates a hash table
+ * @size: size of the array
+ *
+ * Return: pointer to the neewly created hash table
+ */
+
+hash_table_t *hash_table_creat(unsigned long int size)
+{
+	
+}
